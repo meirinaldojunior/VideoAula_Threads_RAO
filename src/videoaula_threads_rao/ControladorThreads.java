f578b6ccc271f7a82cb567875908c156813f8c14
Meirinaldo Júnior <meirinaldo.junior0@gmail.com>
2018-06-10 21:10:24 -0300

    código com comentários sobre as thread

--- src/videoaula_threads_rao/ControladorThreads.java
+++ src/videoaula_threads_rao/ControladorThreads.java
@@ -9,22 +9,30 @@ package videoaula_threads_rao;
  *
  * @author meirinaldojunior
  */
+
+//Extende da classe thread do java
 public class ControladorThreads extends Thread {
 
     private int id;
     private int delay;
     private int repeticao;
 
+    //construtor recebe parametros do main
     public ControladorThreads(int id, int delay, int repeticao) {
         this.id = id;
         this.delay = delay;
         this.repeticao = repeticao;
     }
     
+    //metodo que roda as instruções da thread
     public void run(){
+        
+        //laço de repetição
         for(int i = 0; i<repeticao; i++){
             try {
+                //debuga a execução
                 System.out.println("ID thread: "+id+" | delay: "+delay+" | Repetição: "+repeticao);
+                //delay entre a finalização das threads 
                 Thread.sleep(delay);
             } catch (Exception e) {
                 System.err.println("Erro: "+e.getMessage());
--- src/videoaula_threads_rao/VideoAula_Threads_RAO.java
+++ src/videoaula_threads_rao/VideoAula_Threads_RAO.java
@@ -15,17 +15,20 @@ public class VideoAula_Threads_RAO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //Cria instância das Threads 
         ControladorThreads th1 = new ControladorThreads(1, 10000, 10);
         ControladorThreads th2 = new ControladorThreads(2, 30000, 10);
         ControladorThreads th3 = new ControladorThreads(3, 60000, 10);
         ControladorThreads th4 = new ControladorThreads(4, 80000, 10);
-
+        
+        //inicia as threads
         th1.start();
         th2.start();
         th3.start();
         th4.start();
 
+        //faz a aplicação parar para finalizar as threads
         try {
             th1.join();
             th2.join();
@@ -34,6 +37,7 @@ public class VideoAula_Threads_RAO {
         } catch (Exception e) {
         }
 
+        //identifica que a aplicação terminou..
         System.err.println("TERMINOU A EXECUÇÃO");
     }
 }
